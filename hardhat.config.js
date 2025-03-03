29d7eda374e99b4ad868956a51f54d10301c58ca
Amir of Ekiti <o.osho@outlook.com>
2025-03-03 17:28:06 +0000

    Updated files: added .env to .gitignore and modified project files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // Load environment variables from .env
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL, // Infura/Alchemy URL from .env
+      accounts: [process.env.PRIVATE_KEY] // Deployer's private key from .env
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    }
+  }
 };
